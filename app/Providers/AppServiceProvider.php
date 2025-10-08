96a9663051a1ebdc88559db1d9107d16cc81d6a1
naeemakram4 <naeemakram884@gmail.com>
2025-10-08 13:17:52 +0500

    Updated AppService Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Request::setTrustedProxies(['*']);
+        Request::setTrustedProxies(['*'], Request::HEADER_X_FORWARDED_ALL);
     }
 }
